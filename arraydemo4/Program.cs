7d21d69bbea665911f1c0220d21e95284f6ed4b4
niyati salot <170120107148@git.org.in>
2020-12-30 09:56:12 +0530

    Two Dimension Array

--- arraydemo4/Program.cs
+++ arraydemo4/Program.cs
@@ -6,10 +6,22 @@ namespace arraydemo4
     {
         static void Main(string[] args)
         {
+            foreachDemo();
+        }
+        static void foreachDemo()
+        {
+            int[] data = new int[5] {1,3,5,7,9};
+            foreach(int item in data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void arraydemo1()
+        {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Enter 5 values to find out sum and average of them:");
             int[] data = new int[5];
-            int sum = 0;
+            double sum = 0;
             double avg;
             for (int i = 0; i < data.Length; i++)
             {
---
+++ arraydemo5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace arraydemo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[,] arr = new int[2, 3] { { 100, 200, 300 }, { 4, 5, 6 } };
+            /*Console.WriteLine("0,0 position contains = " + arr[0, 0]);
+            Console.WriteLine("0,1 position contains = " + arr[0, 1]);
+            Console.WriteLine("0,2 position contains = " + arr[0, 2]);
+            Console.WriteLine("1,0 position contains = " + arr[1, 0]);*/
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(arr[i, j]);
+                }
+            }
+            int r = arr.Rank;
+            Console.WriteLine($"rank is {r}");
+            int l = arr.GetLength(1);
+            Console.WriteLine($"Length is {l}");
+            int l1 = arr.GetLength(0);
+            Console.WriteLine($"length for 0 is {l1}");
+        }
+    }
+}
